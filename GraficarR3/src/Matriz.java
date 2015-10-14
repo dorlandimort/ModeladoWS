0d18798a190d09977c7acdf309d97d04f31ebdac
dorlandimort <dorlandimort@hotmail.com>
2015-10-14 15:53:53 -0500

    puntotes extra

---
+++ GraficarR3/src/Matriz.java
@@ -0,0 +1,12 @@
+
+public interface Matriz {
+	
+	public double[] trasladar(double[] puntoInicial, double[] vectorTraslacion);
+	
+	public double[] escalar(double[] puntoInicial, int dimension, double k);
+	
+	public double[] rotar(double[] puntoInicial, double angulo, int dimension);
+	
+	public double[] transformar(double[] puntoInicial);
+
+}
